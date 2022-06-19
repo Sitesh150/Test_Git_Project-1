75da18480a9df78f447d0cb852ba08b78bff7ac6
Sitesh <vishwakarma.kr80@gmail.com>
2022-06-19 12:34:10 +0530

    Updated in slveBranch-2

---
+++ src/test/java/test/Child_BranchTest.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Child_BranchTest {
+	public static void main(String[] args) {
+        System.out.println("This is Child Branch");
+	}
+}
--- src/test/java/test/Welcome_test.java
+++ src/test/java/test/Welcome_test.java
@@ -4,5 +4,6 @@ public class Welcome_test {
 
 	public static void main(String[] args) {
        System.out.println("Welcom in new Github");
+       System.out.println("ok");
 	}
 }
